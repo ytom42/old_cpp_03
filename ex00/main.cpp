9099869b3cb2af999fdc897378f7a2a09b29cfb9
yutaro <ytom42tk@gmail.com>
2021-11-15 17:04:05 +0900

    update ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ytomiyos <ytomiyos@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 10:26:54 by ytomiyos          #+#    #+#             */
-/*   Updated: 2021/11/14 13:25:42 by ytomiyos         ###   ########.fr       */
+/*   Updated: 2021/11/15 16:39:15 by ytomiyos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,10 @@
 int main()
 {
 	ClapTrap c1("c1");
-	ClapTrap c2("c2");
 
 	c1.attack("hoge");
-	c2.attack("fuga");
 	c1.takeDamage(5);
-	c2.takeDamage(10);
 	c1.beRepaired(1);
-	c2.beRepaired(100);
+
+	return (0);
 }
