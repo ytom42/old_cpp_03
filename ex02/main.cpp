59bdce815a3c1748ca6c67779de3caa7a3c70602
yutaro <ytom42tk@gmail.com>
2021-11-15 17:04:39 +0900

    update ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ytomiyos <ytomiyos@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 10:26:54 by ytomiyos          #+#    #+#             */
-/*   Updated: 2021/11/14 16:38:08 by ytomiyos         ###   ########.fr       */
+/*   Updated: 2021/11/15 16:53:40 by ytomiyos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,13 @@
 int main()
 {
 	ClapTrap c1("c1");
-	ClapTrap c2("c2");
-
 	ScavTrap s1("s1");
-	ScavTrap s2("s2");
-
 	FragTrap f1("f1");
-	FragTrap f2("f2");
-
+	
 	f1.attack("hoge");
-	f2.attack("fuga");
-	f1.highFivesGuys();
-	f2.highFivesGuys();
 	f1.takeDamage(20);
-	f2.takeDamage(30);
 	f1.beRepaired(10);
-	f2.beRepaired(10);
+	f1.highFivesGuys();
 
 	return (0);
 }
